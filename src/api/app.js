050828c93694f323244db5b068cef74ce394bfd2
Douglas-Santana <douguinhodns@gmail.com>
2021-12-07 16:06:06 -0300

    adicionado rota e criado arquivo nao emplementado  para busca de receitas

--- src/api/app.js
+++ src/api/app.js
@@ -16,5 +16,6 @@ app.get('/', (request, response) => {
 app.post('/users', routes.createUsers);
 app.post('/login', routes.login);
 app.post('/recipes', validAuth, routes.createRecipes);
+app.get('/recipes', validAuth, routes.getRecipes);
 
 module.exports = app;
--- src/api/routes.js
+++ src/api/routes.js
@@ -1,5 +1,6 @@
 const createUsers = require('../controllers/createUser');
 const login = require('../controllers/login');
 const createRecipes = require('../controllers/createRecipe');
+const getRecipes = require('../controllers/getRecipe');
 
-module.exports = { createUsers, login, createRecipes };
+module.exports = { createUsers, login, createRecipes, getRecipes };
