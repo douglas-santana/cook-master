85412461ed409b04e52213a6a8304f5509ed4c2d
Douglas-Santana <douguinhodns@gmail.com>
2021-12-07 18:04:51 -0300

    adicionado rota e arquivo para editar uma receita

--- src/api/routes.js
+++ src/api/routes.js
@@ -3,5 +3,13 @@ const login = require('../controllers/login');
 const createRecipes = require('../controllers/createRecipe');
 const getAllRecipes = require('../controllers/getAllRecipes');
 const getEspecifyRecipe = require('../controllers/getEspecifyRecipe');
+const editRecipe = require('../controllers/editRecipe');
 
-module.exports = { createUsers, login, createRecipes, getAllRecipes, getEspecifyRecipe };
+module.exports = {
+  createUsers,
+  login,
+  createRecipes,
+  getAllRecipes,
+  getEspecifyRecipe,
+  editRecipe,
+};
